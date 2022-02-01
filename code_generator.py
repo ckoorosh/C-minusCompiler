972057c48d5d711922faed3842bb78e69181144b
pourya-momtaz <pouryamz19@gmail.com>
2022-02-01 18:38:43 +0330

    added relop_routine

--- code_generator.py
+++ code_generator.py
@@ -84,3 +84,14 @@ class CodeGenerator:
         except IndexError:
             pass
 
+
+    def relop_routine(self, input_token):
+        try:
+            result = self.get_temp()
+            operand1 = self.semantic_stack.pop()
+            operand2 = self.semantic_stack.pop()
+            op = self.semantic_stack.pop()
+            self.add_code((op, operand1, operand2, result))
+            self.semantic_stack.append(result)
+        except IndexError:
+            pass
