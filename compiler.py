f28f9a5f1b1f379a46364155c3164e6172f23dde
ckoorosh <53394330+ckoorosh@users.noreply.github.com>
2022-02-03 18:37:58 +0330

    Adding input2

--- compiler.py
+++ compiler.py
@@ -9,8 +9,9 @@ def main(input_path):
     scanner = Scanner(input_path)
     parser = Parser(scanner)
     parser.parse()
-
+    parser.save_parse_tree()
+    parser.save_errors()
 
 
 if __name__ == '__main__':
-    main('input.txt')
+    main('input2.txt')
